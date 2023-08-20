ab2b3058a42a02928b43d4af12695e2a7edef908
okolobaiev <okolobaiev@gmail.com>
2023-08-20 16:10:13 +0300

    first commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="clean_folder",
+    version="1",
+    description="Organizing files in folder",
+    url="https://github.com/okolobaiev/hw_6",
+    author="Oleh K.",
+    author_email="o.kolobaiev@gmail.com",
+    license="MIT",
+    packages=["clean_folder"],
+)
